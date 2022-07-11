6920eb5cf3121fd8b41c7f3a1712be76342c987e
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2022-07-12 00:20:50 +0300

    4

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./js/gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const paretteContainer = document.querySelector('.gallery');
+const cre = createGalleryItems(galleryItems);
+
+paretteContainer.insertAdjacentHTML('beforeend', cre);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    }).join('');
+}
